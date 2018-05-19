79fd9881aceba3573c185df3709aa13af04e27b5
riskw <riskic44@gmail.com>
2018-05-19 11:01:05 +0700

    sek sek

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -1,29 +1,37 @@
 <html>
- <head>
-   <title>Dashboard</title>
-   <link rel="stylesheet" type="text/css" href="../css/allin.css" />
- </head>
- <body>
- <div class="header">
-   <a href="#default" class="header">The Dashboard</a>
-   <div class="header-right">
-     <a class="#about" href="../func/logout.php">Logout</a>
-   </div>
- </div>
- <div class="wreaper">
-			<aside class="kiri">
-				<li>
-					<ol><a href="dashboard.php">Dashboard</a></ol>
-					<ol><a href="hitung.php">Hitung Gula Darah</a></ol>
-					<ol><a href="makanan.php">List Makanan</a></ol>
-					<ol><a href="olahraga.php">List Olahraga</a></ol>
-				</li>
-			</aside>
+
+<head>
+  <title>Dashboard</title>
+  <link rel="stylesheet" type="text/css" href="../css/allin.css" />
+</head>
+
+<body>
+  <div class="header">
+    <a href="#default" class="header">The Dashboard</a>
+    <div class="header-right">
+      <a class="#about" href="../func/logout.php">Logout</a>
+    </div>
   </div>
- <div class="footer">
-   <p>
-     This is Footer
-   </p>
- </div>
+  <div class="row">
+    <div class="column side">
+      <a href="dashboard.php">Dashboard</a>
+      <a href="hitung.php">Hitung Gula Darah</a>
+        <a href="makanan.php">List Makanan</a>
+        <a href="olahraga.php">List Olahraga</a>
+    </div>
+    <div class="column middle">
+      <h2>Main Content</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus
+        quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus
+        quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
+    </div>
+  </div>
+  <div class="footer">
+    <p>
+      This is Footer
+    </p>
+  </div>
 </body>
+
 </html>
